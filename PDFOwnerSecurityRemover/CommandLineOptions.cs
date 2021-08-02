33d0dd66344d6b455202ab5bc2510647f4c6dd02
hardrive9000 <hardrive9000@users.noreply.github.com>
2021-08-02 13:22:42 -0300

    Initial commit

---
+++ PDFOwnerSecurityRemover/CommandLineOptions.cs
@@ -0,0 +1,11 @@
+﻿using CommandLine;
+using System.Collections.Generic;
+
+namespace PDFOwnerSecurityRemover
+{
+    public sealed class CommandLineOptions
+    {
+        [Option('f', "file", Required = true, HelpText = "PDF file(s) to remove owner permissions", Separator = ' ')]
+        public IEnumerable<string> InputFiles { get; set; }
+    }
+}
---
+++ PDFOwnerSecurityRemover/Program.cs
@@ -0,0 +1,37 @@
+﻿using CommandLine;
+using iText.Kernel.Pdf;
+using System.IO;
+
+namespace PDFOwnerSecurityRemover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<CommandLineOptions>(args).WithParsed(opts => Execute(opts));
+        }
+
+        private static void Execute(CommandLineOptions opts)
+        {
+            foreach (string file in opts.InputFiles)
+            {
+                if (File.Exists(file))
+                {
+                    string fileName = Path.Combine(Path.GetDirectoryName(file), "unprotected_" + Path.GetFileName(file));
+
+                    using PdfReader pdfReader = new PdfReader(file);
+                    pdfReader.SetUnethicalReading(true);
+                    using PdfDocument protectedPdfDocument = new PdfDocument(pdfReader);
+
+                    using PdfWriter pdfWriter = new PdfWriter(fileName);
+                    using PdfDocument unprotectedPdfDocument = new PdfDocument(pdfWriter);
+
+                    protectedPdfDocument.CopyPagesTo(1, protectedPdfDocument.GetNumberOfPages(), unprotectedPdfDocument);
+
+                    protectedPdfDocument.Close();
+                    unprotectedPdfDocument.Close();
+                }
+            }
+        }
+    }
+}
